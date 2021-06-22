83c241a92b2735472ca005203f7e683238b60534
Roman Ivanov <roivanoff@gmail.com>
2021-06-22 09:50:41 +0300

    first

---
+++ src/test/java/com/faasj/console/ConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faasj.console;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
